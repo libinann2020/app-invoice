98e3873a4d35ffdb0e075e89baaf2401a51163dd
libin ann babu <libin.babu@altd.in>
2023-04-25 08:54:16 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\InvoiceController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('/get_all_invoice', [InvoiceController::class, 'get_all_invoice']);
+Route::get('/search_invoice', [InvoiceController::class, 'search_invoice']);
+Route::get('/create_invoice', [InvoiceController::class, 'create_invoice']);
